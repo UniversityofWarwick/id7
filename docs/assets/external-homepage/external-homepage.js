e1ca825b1a90d6700284f4ba2b457181a7167dca
Mat Mannion <m.mannion@warwick.ac.uk>
2015-03-18 18:07:54 +0000

    Tweaks

---
+++ docs/assets/external-homepage/external-homepage.js
@@ -0,0 +1,23 @@
+(function ($) {
+    'use strict';
+
+    // Smooth scroll
+    $('.carousel-nav a[href^="#"]').on('click', function(e) {
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // store hash
+        var hash = this.hash;
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 300, function(){
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+    });
+
+    $('[data-toggle="popover"]').popover().on('click', function(e) { e.preventDefault(); });
+})(jQuery);
