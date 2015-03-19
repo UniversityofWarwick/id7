fac0de15fae29d32b654bac5f3cf6c43c5b12562
Mat Mannion <mat.mannion@gmail.com>
2015-03-19 13:18:33 +0000

    SVG version of logo

--- docs/assets/external-homepage/external-homepage.js
+++ docs/assets/external-homepage/external-homepage.js
@@ -1,23 +1,27 @@
 (function ($) {
     'use strict';
 
-    // Smooth scroll
-    $('.carousel-nav a[href^="#"]').on('click', function(e) {
-        // prevent default anchor click behavior
-        e.preventDefault();
+    $(function() {
+      $('body').scrollspy({ target: '.carousel-nav' });
 
-        // store hash
-        var hash = this.hash;
+      // Smooth scroll
+      $('.carousel-nav a[href^="#"]').on('click', function(e) {
+          // prevent default anchor click behavior
+          e.preventDefault();
 
-        // animate
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 300, function(){
-            // when done, add hash to url
-            // (default click behaviour)
-            window.location.hash = hash;
-        });
+          // store hash
+          var hash = this.hash;
+
+          // animate
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 300, function(){
+              // when done, add hash to url
+              // (default click behaviour)
+              window.location.hash = hash;
+          });
+      });
+
+      $('[data-toggle="popover"]').popover().on('click', function(e) { e.preventDefault(); });
     });
-
-    $('[data-toggle="popover"]').popover().on('click', function(e) { e.preventDefault(); });
 })(jQuery);
