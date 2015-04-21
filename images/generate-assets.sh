2537d7f7bdcb7178b4214e73580226227d0fb28d
Mat Mannion <m.mannion@warwick.ac.uk>
2015-04-21 10:34:33 +0100

    Rename -homepage- images to -wide- as they are included in the main ID7 build

--- images/generate-assets.sh
+++ images/generate-assets.sh
@@ -6,14 +6,14 @@
 ./svg-to-png.sh masthead-logo-bleed-xs@2x.svg 3360 600 masthead-logo-bleed-xs@2x.png
 ./svg-to-png.sh masthead-logo-bleed-xs.svg 1680 300 masthead-logo-bleed-xs.png
 
-./svg-to-png.sh masthead-logo-bleed-homepage-xs@2x.svg 3360 600 masthead-logo-bleed-homepage-xs@2x.png
-./svg-to-png.sh masthead-logo-bleed-homepage-xs.svg 1680 300 masthead-logo-bleed-homepage-xs.png
-./svg-to-png.sh masthead-logo-bleed-homepage-sm@2x.svg 1984 160 masthead-logo-bleed-homepage-sm@2x.png
-./svg-to-png.sh masthead-logo-bleed-homepage-sm.svg 992 80 masthead-logo-bleed-homepage-sm.png
-./svg-to-png.sh masthead-logo-bleed-homepage-md@2x.svg 2400 160 masthead-logo-bleed-homepage-md@2x.png
-./svg-to-png.sh masthead-logo-bleed-homepage-md.svg 1200 80 masthead-logo-bleed-homepage-md.png
-./svg-to-png.sh masthead-logo-bleed-homepage-lg@2x.svg 6000 160 masthead-logo-bleed-homepage-lg@2x.png
-./svg-to-png.sh masthead-logo-bleed-homepage-lg.svg 3000 80 masthead-logo-bleed-homepage-lg.png
+./svg-to-png.sh masthead-logo-bleed-wide-xs@2x.svg 3360 600 masthead-logo-bleed-wide-xs@2x.png
+./svg-to-png.sh masthead-logo-bleed-wide-xs.svg 1680 300 masthead-logo-bleed-wide-xs.png
+./svg-to-png.sh masthead-logo-bleed-wide-sm@2x.svg 1984 160 masthead-logo-bleed-wide-sm@2x.png
+./svg-to-png.sh masthead-logo-bleed-wide-sm.svg 992 80 masthead-logo-bleed-wide-sm.png
+./svg-to-png.sh masthead-logo-bleed-wide-md@2x.svg 2400 160 masthead-logo-bleed-wide-md@2x.png
+./svg-to-png.sh masthead-logo-bleed-wide-md.svg 1200 80 masthead-logo-bleed-wide-md.png
+./svg-to-png.sh masthead-logo-bleed-wide-lg@2x.svg 6000 160 masthead-logo-bleed-wide-lg@2x.png
+./svg-to-png.sh masthead-logo-bleed-wide-lg.svg 3000 80 masthead-logo-bleed-wide-lg.png
 
 ./svg-to-png.sh id6a-logotype@2x.svg 272 52 id6a-logotype@2x.png false
 ./svg-to-png.sh id6a-logotype.svg 136 26 id6a-logotype.png false
