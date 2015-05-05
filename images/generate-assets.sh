d5f56ec482adcf185103955d8eed65b5825ec13f
Mat Mannion <m.mannion@warwick.ac.uk>
2015-05-05 10:22:29 +0100

    ID-126 Provide SVG versions of divider in multiple colours

--- images/generate-assets.sh
+++ images/generate-assets.sh
@@ -22,3 +22,11 @@
 
 ./svg-to-png.sh logo-with-descriptor-purple.svg 600 398 logo.png false
 ./svg-to-png.sh logo-purple.svg 600 347 logo-no-descriptor.png false
+
+./svg-to-png.sh logo-divider-purple.svg 1130 40 logo-divider-purple.png false
+./svg-to-png.sh logo-divider-gray.svg 1130 40 logo-divider-gray.png false
+./svg-to-png.sh logo-divider-gold.svg 1130 40 logo-divider-gold.png false
+./svg-to-png.sh logo-divider-orange.svg 1130 40 logo-divider-orange.png false
+./svg-to-png.sh logo-divider-red.svg 1130 40 logo-divider-red.png false
+./svg-to-png.sh logo-divider-green.svg 1130 40 logo-divider-green.png false
+./svg-to-png.sh logo-divider-blue.svg 1130 40 logo-divider-blue.png false
