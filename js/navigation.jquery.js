f769ad1755cdf0462754be1b89121b2e2ad6c5d2
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2016-10-21 14:02:33 +0100

    ID-209 Navigate to tabs where available, using bootstrap convention

--- js/navigation.jquery.js
+++ js/navigation.jquery.js
@@ -334,5 +334,19 @@
 
   $(function () {
     $('.id7-navigation').id7Navigation();
+
+    // jump to a tab if specified
+    var url = document.location.toString();
+    if (url.match('#') && url.split('#')[1].length) {
+      var $tabLink = $('.nav-tabs a[href="#' + url.split('#')[1] + '"]');
+      if ($tabLink) {
+        $tabLink.tab('show')
+      }
+    }
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown.bs.tab', function (e) {
+      window.location.hash = e.target.hash;
+    });
   });
 })(jQuery);
