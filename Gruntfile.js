60564606b8283555618487cf775525493e678f0d
Mat Mannion <M.Mannion@warwick.ac.uk>
2017-08-14 14:37:18 +0100

    ID-236 Change autoprefixer settings to something a bit more modern

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,16 +197,7 @@ module.exports = function (grunt) {
 
     autoprefixer: {
       options: {
-        browsers: [
-          "Android 2.3",
-          "Android >= 4",
-          "Chrome >= 20",
-          "Firefox >= 24",
-          "Explorer >= 8",
-          "iOS >= 6",
-          "Opera >= 12",
-          "Safari >= 6"
-        ]
+        browsers: [ '> 1% in GB', 'last 2 versions', 'IE 9' ]
       },
       core: {
         options: {
