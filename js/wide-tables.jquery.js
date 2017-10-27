bcff0e524686988555f32937d99f580abcd02175
Matthew Jones <matthew.jones@warwick.ac.uk>
2017-10-27 12:07:03 +0100

    ID-239 - Fix linting

--- js/wide-tables.jquery.js
+++ js/wide-tables.jquery.js
@@ -88,7 +88,7 @@
 
     $.extend(WideTables.prototype, {
       findWideTables: function findWideTables($container) {
-        return $container.find('table').filter(function() {
+        return $container.find('table').filter(function () {
           return $(this).parents('.no-wide-tables').length === 0;
         }).filter(function () {
           var $table = $(this);
