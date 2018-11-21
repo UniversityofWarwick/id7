49f7e3a747702c920c5fc1f474e8ffc9c09cbddd
Adam Williams <lol768@users.noreply.github.com>
2018-11-21 14:25:29 +0000

    ID-250: Switch to vector (SVG) backgrounds for desktop Safari

--- js/safari-user-agent-detect.modernizr.js
+++ js/safari-user-agent-detect.modernizr.js
@@ -12,5 +12,10 @@
 
     return ua.indexOf('Safari/') >= 0 && ua.indexOf('Chrome/') == -1;
   });
+
+  Modernizr.addTest('safari-idevice', function () {
+      var ua = window.navigator.userAgent;
+      return ua.indexOf('Safari/') >= 0 && ua.indexOf('iP') >= 0;
+  });
 })();
 
