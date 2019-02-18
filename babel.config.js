a18cf00b2265271c9b1b77063eac19e4e29bc361
Mat Mannion <mat.mannion@gmail.com>
2019-02-18 15:39:00 +0000

    Lock dependencies (and upgrade to latest)

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = function(api) {
+  // Cache results until NODE_ENV changes
+  api.cache.using(() => process.env.NODE_ENV);
+
+  return {
+    "presets": ["@babel/preset-env"]
+  };
+};
