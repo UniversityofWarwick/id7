8639f7a9410d49a8581af050bd290a83f226c4a0
Adam Williams <lol768@users.noreply.github.com>
2019-12-09 16:06:29 +0000

    Swap over to window.matchMedia, still using resize event for now
    TODO: Listeners on MediaQueryList objects

--- js/screen-sizes.js
+++ js/screen-sizes.js
@@ -1,22 +1,25 @@
 /* eslint-env browser */
 import _ from 'lodash-es';
-import FeatureDetect from './feature-detect';
 
 const ScreenSizes = [
   {
-    name: 'lg', test() { return FeatureDetect.mq('only all and (min-width: 1200px)'); }, container: 1170,
+    name: 'lg', matcher() { return window.matchMedia('(min-width: 1200px)'); }, container: 1170,
   },
   {
-    name: 'md', test() { return FeatureDetect.mq('only all and (min-width: 992px)'); }, container: 970,
+    name: 'md', matcher() { return window.matchMedia('(min-width: 992px)'); }, container: 970,
   },
   {
-    name: 'sm', test() { return FeatureDetect.mq('only all and (min-width: 768px)'); }, container: 750,
+    name: 'sm', matcher() { return window.matchMedia('(min-width: 768px)'); }, container: 750,
   },
   {
-    name: 'xs', test() { return true; },
+    name: 'xs', matcher() { return window.matchMedia('(max-width: 768px)'); },
   },
 ];
 
 export default function currentScreenSize() {
-  return _.find(ScreenSizes, (screenConfig) => screenConfig.test());
+  return _.find(ScreenSizes, screenConfig => screenConfig.matcher().matches);
+}
+
+export default function allScreenSizes() {
+  return ScreenSizes;
 }
